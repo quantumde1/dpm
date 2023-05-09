d34cd07c1778155c5827957a341b12a184f3041b
quantumde1 <meserakovarsenij810@gmail.com>
2023-05-09 10:57:00 +0300

    Fixed file with installed packages

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,34 @@
 #include <fstream>
 #include <iostream>
+#include <cstring>
+#include <sstream>
+#include <filesystem>
 
 using namespace std;
+
+void eraseFileLine(std::string path, std::string eraseLine) {
+    std::string line;
+    std::ifstream fin;
+    
+    fin.open(path);
+    // contents of path must be copied to a temp file then
+    // renamed back to the path file
+    std::ofstream temp;
+    temp.open("/var/lib/dpm/tmp.txt");
+
+    while (getline(fin, line)) {
+        // write all lines to temp other than the line marked for erasing
+        if (line != eraseLine)
+            temp << line << std::endl;
+    }
+
+    temp.close();
+    fin.close();
+
+    // required conversion for remove and rename functions
+    //remove("/var/lib/dpm/tmp.txt");
+    //std::filesystem::rename("/var/lib/dpm/tmp.txt", "/var/lib/dpm/installedpkg.txt");
+}
 
 int main(int argc, char **argv) {
     unsigned int cur_line = 0;
@@ -9,12 +36,11 @@ int main(int argc, char **argv) {
     file.open("/var/lib/dpm/installedpkg.txt");
     string line;
     string deleteline = argv[1];
-    string path = "/usr/bin/";
-    string sec = path+deleteline;
     while(getline(file, line)) {
         cur_line++;
         if (line.find(deleteline, 0) != string::npos) {
             cout << "found" << endl;
+            eraseFileLine("/var/lib/dpm/installedpkg.txt", argv[1]);
         }
     }
 }
--- source/remove.d
+++ source/remove.d
@@ -8,11 +8,13 @@ import std.process;
 
 void rm_pkg(string packagename) {
     auto ps = execute(["/usr/bin/dpm_search", "/usr/bin/"~packagename]);
+    writeln(ps);
     writeln(ps.output);
     if (ps.output == "found\n") {
         writeln("Installed, so will be removed");
         auto path = "/usr/bin/"~packagename;
         path.remove;
+        std.file.rename("/var/lib/dpm/tmp.txt", "/var/lib/dpm/installedpkg.txt");
         rmdirRecurse("/etc/"~packagename);
     }
     else {
